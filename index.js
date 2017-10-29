ab701605ce3850c615d3efca7756f27e9c06737c
Timothy-2017 <timothy.eubank@gmail.com>
2017-10-29 13:45:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (name) {return name.toLowerCase()});
+}
+
+function nameToAttributes(drivers) {
+  const nameAttributesArr = []
+  drivers.map(function (name) {
+      nameArr = name.split(" ");
+      nameObj = {};
+      nameObj["firstName"] = nameArr[0];
+      nameObj["lastName"] = nameArr[1];
+      nameAttributesArr.push(nameObj);
+    }
+  );
+  return nameAttributesArr;
+}
+
+function attributesToPhrase(drivers) {
+  const phraseArr = [];
+  drivers.map(function(driver) {
+      phraseArr.push(`${driver['name']} is from ${driver['hometown']}`);
+    }
+  );
+  return phraseArr;
+}
